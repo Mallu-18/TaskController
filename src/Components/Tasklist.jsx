a0dee2bb5c0510b1dac248a6e16a7d378560ba9b
Mallu-18 <mallikarjunbiradar003@gmail.com>
2025-10-18 10:41:49 +0530

    third commit

--- src/Components/Tasklist.jsx
+++ src/Components/Tasklist.jsx
@@ -7,7 +7,7 @@ export default function Tasklist({tasks , updateTask, deleteTask}) {
   return (
     <ul>
       {tasks.map((task, index)=> (
-        <li key={index}>
+        <li key={index} className={task.completed ? "completed" : ""}>
           <div>
             <span>{task.text}
               <small>({task.priority} , {task.category})</small>
